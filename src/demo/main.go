073a673f6daf871b450b23ececb6aeebbf1e69a7
yide <yide.me@icloud.com>
2019-03-30 23:47:16 +0800

    http server

--- src/demo/main.go
+++ src/demo/main.go
@@ -10,12 +10,27 @@ func main() {
 
 	fmt.Println("Hello Tyrion!")
 
+	a := &http.Options{
+		Port: 12354,
+	}
+
+	b := new(http.Options)
+	*b = *a
+	b.Port = 54321
+
+	fmt.Print(a)
+	fmt.Print(b)
+}
+
+func runHttpServer() {
 	app := http.New()
 
 	app.Init(&http.Options{
 		IP:   "",
 		Port: 3210,
 	})
+
+	app.InitByConfig("project.ini")
 
 	http2.HandleFunc("/demo", func(writer http2.ResponseWriter, request *http2.Request) {
 		fmt.Fprintf(writer, "Hello")
@@ -24,4 +39,5 @@ func main() {
 	if err := app.Run(); err != nil {
 		panic(err.Error())
 	}
+
 }
