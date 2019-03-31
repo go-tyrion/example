042be9ffbd20552262cfe1f5a2920f637b13aefc
yide <yide.me@icloud.com>
2019-03-31 15:37:39 +0800

    http server

--- src/demo/main.go
+++ src/demo/main.go
@@ -3,37 +3,24 @@ package main
 import (
 	"fmt"
 	"lib/server/http"
-	http2 "net/http"
 )
 
-func main() {
-
+func init() {
 	fmt.Println("Hello Tyrion!")
-
-	a := &http.Options{
-		Port: 12354,
-	}
-
-	b := new(http.Options)
-	*b = *a
-	b.Port = 54321
-
-	fmt.Print(a)
-	fmt.Print(b)
+}
+func main() {
+	runHttpServer()
 }
 
 func runHttpServer() {
 	app := http.New()
 
 	app.Init(&http.Options{
-		IP:   "",
 		Port: 3210,
 	})
 
-	app.InitByConfig("project.ini")
-
-	http2.HandleFunc("/demo", func(writer http2.ResponseWriter, request *http2.Request) {
-		fmt.Fprintf(writer, "Hello")
+	app.Get("/demo", func(ctx *http.Context) {
+		ctx.String(200, "Hello")
 	})
 
 	if err := app.Run(); err != nil {
